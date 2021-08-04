5079d958a1bf4fe5441aa239911973e27b974bcf
Yudi Candra <yudhichandra02@gmail.com>
2021-08-04 21:59:17 +0700

    add length of column

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -27,6 +27,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        Schema::defaultStringLength(191);
         config(['app.locale' => 'id']);
         Carbon::setLocale('id');
         date_default_timezone_set('Asia/Jakarta');
